a0420c25073c960ad5d4b6ce03dc17c708d9848d
cleminator <alvinclemens@gmail.com>
2020-11-14 12:40:41 -0700

    exercise.html is joined and running on the server

--- Develop/server.js
+++ Develop/server.js
@@ -23,10 +23,11 @@ app.get('/', (request,response) => {
     response.sendFile(path.join(__dirname, + 'index.html'));
 })
 
-app.get('/exercise', (request,response) => {
-    console.log(__dirname + 'exercise.html')
-    response.sendFile(path.join(__dirname, + 'public/exercise.html'));
+app.get('/exercise', (request, response) => {
+    console.log(__dirname + '/exercise.html')
+    response.sendFile(path.join(__dirname, 'public/exercise.html'));
 })
+
 app.get('/stats', (request, response) => {
     console.log(__dirname + '/stats.html')
     response.sendFile(path.join(__dirname, 'public/stats.html'));
