4c9d866e4602dfca8b9b17d721a107b70c3bce0e
cleminator <alvinclemens@gmail.com>
2020-11-14 12:16:43 -0700

    server runs with index.html and stats.html

--- Develop/server.js
+++ Develop/server.js
@@ -20,16 +20,16 @@ app.use(logger("dev"));
 
 app.get('/', (request,response) => {
     console.log(__dirname + 'index.html')
-    response.sendFile(path,join(__dirname + 'index.html'));
+    response.sendFile(path.join(__dirname, + 'index.html'));
 })
 
 app.get('/exercise', (request,response) => {
     console.log(__dirname + 'exercise.html')
-    response.sendFile(path,join(__dirname + 'index.html'));
+    response.sendFile(path.join(__dirname, + 'public/exercise.html'));
 })
-app.get('/stats', (request,response) => {
-    console.log(__dirname + 'stats.html')
-    response.sendFile(path,join(__dirname + 'index.html'));
+app.get('/stats', (request, response) => {
+    console.log(__dirname + '/stats.html')
+    response.sendFile(path.join(__dirname, 'public/stats.html'));
 })
 
 
