d6e4fcc69ff5bdfcfca5995c9d025ef47541d6f5
cleminator <alvinclemens@gmail.com>
2020-11-14 12:06:15 -0700

    server is running and now pulls the index.html

--- Develop/server.js
+++ Develop/server.js
@@ -1,23 +1,27 @@
+
 const express = require("express")
 const app = express();
 const PORT = process.env.PORT || 8080;
 var mongoose = require("mongoose");
+var logger = require("morgan");
 const path = require("path")
-const apiRoutes = require(path.join(__dirname, "./routes/api"))
-const db = require("./modules");
+// const apiRoutes = require(path.join(__dirname, "./routes/api"))
 
 
 
 //Middleware for data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(__dirname + "/Develop/public"));
-// app.use(logger("dev"));
+app.use(express.static(__dirname + "/public"));
+app.use(logger("dev"));
 
 // require("./routes/api.js")(app)
 // app.use(apiRoutes)
 
-
+app.get('/', (request,response) => {
+    console.log(__dirname + 'index.html')
+    response.sendFile(path,join(__dirname + 'index.html'));
+})
 
 
 
