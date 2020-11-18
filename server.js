8854b89664de73c3f03afb9562161e7c8f836777
cleminator <alvinclemens@gmail.com>
2020-11-17 20:33:50 -0700

    will not use this code

--- server.js
+++ server.js
@@ -7,23 +7,11 @@ var logger = require("morgan");
 const path = require("path")
 const apiRoutes = require(path.join(__dirname, "/routes/routes"))
 mongoose.connect 
-<<<<<<< HEAD:server.js
-mongoose.connect(
-    process.env.MONGODB_URI || ('mongodb://localhost/workitout',
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false
-    })
-  );
-=======
 process.env.MONGODB_URI || ('mongodb://localhost/Workitout',
     {
         useNewUrlParser: true,
         useFindAndModify: false
     });
->>>>>>> parent of bd9ea1b... lets see if this deploys correctly:Develop/server.js
 
 
 
