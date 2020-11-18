e10d7e938de8fcc596362fdf417ff0b9e905b7e9
cleminator <alvinclemens@gmail.com>
2020-11-17 17:29:22 -0700

    put request is not working

--- Develop/routes/routes.js
+++ Develop/routes/routes.js
@@ -2,14 +2,6 @@ const express = require("express");
 const db = require("../model");
 const router = express.Router()
 
-// Put the post in here
-router.post("/api/workouts", ({ body }, res) => {
-    db.Workout.create(body).then((workitoutdb => {
-        res.json(workitoutdb);
-    })).catch(err => {
-        res.json(err);
-    });
-});
 router.get("/api/workouts", (req, res) => {
     db.Workout.find({}).then(workitoutdb => {
         res.json(workitoutdb);
@@ -18,6 +10,14 @@ router.get("/api/workouts", (req, res) => {
     });
 });
 
+// Put the post in here
+router.post("/api/workouts", ({ body }, res) => {
+    db.Workout.create(body).then(workitoutdb => {
+        res.json(workitoutdb);
+    }).catch(err => {
+        res.json(err);
+    });
+});
 
 // adding exersize to main screen
 router.put("api/workouts/id", (req,res) => {
